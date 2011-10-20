d7e557f6548d85414e654c558ef48a3480755305
Jacul <jaculing@gmail.com>
2011-10-20 00:09:07 -0400

    Initial Commit

---
+++ CSC630_2D_Video_Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CSC630_2D_Video_Game
+//
+//  Created by zhu xiangdong on 11-10-20.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
